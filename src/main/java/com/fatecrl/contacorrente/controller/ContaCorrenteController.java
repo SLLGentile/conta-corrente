d1bfd2e0fa97e9ac0cc806c22bd37eb8d950f42e
Sergio Gentile <sergiogentilesi@gmail.com>
2024-09-05 22:09:58 -0300

    Criação de model e service

---
+++ src/main/java/com/fatecrl/contacorrente/controller/ContaCorrenteController.java
@@ -0,0 +1,24 @@
+package com.fatecrl.contacorrente.controller;
+
+/*01 import org.springframework.web.bind.annotation.GetMapping;*/
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/conta-corrente")
+public class ContaCorrenteController {
+
+/*  
+
+01-
+    @GetMapping("/obter")
+    public String get(){
+    return "conteudo";
+
+}
+    ResponseEntity <Conta> podemos enviar "404", "200", xml, respostas, comunicação da api.
+*/
+
+
+
+}
